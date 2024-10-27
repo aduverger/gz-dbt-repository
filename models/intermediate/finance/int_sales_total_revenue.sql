2b8f73cbb8afbe8380ff393ed069a5116c99eb21
Alexandre Duverger <alex@daikon.eco>
2024-10-27 16:29:08 +0000

    add intermediate table with sales + products

--- models/intermediate/finance/int_sales_total_revenue.sql
+++
@@ -1,9 +0,0 @@
-with
-    sales as (select * from {{ ref("stg_crm__sales") }}),
-
-    sales_with_total_revenue as (
-        select *, (quantity * revenue) as total_revenue, from sales
-    )
-
-select *
-from sales_with_total_revenue
